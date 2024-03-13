0180e7f7602091f8db1a27157d32ba9c757bf6fa
markus.heinonen <make.heinonen@outlook.com>
2024-03-13 04:25:50 +0200

    socket io

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,36 +2,47 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
 
 require('dotenv').config();
 
 const middlewares = require('./middlewares');
 const api = require('./api');
+const { Server } = require("socket.io");
+const io = require('socket.io')(server);
 
-// const app = express();
-// const http = require('http');
-// const server = http.createServer(app);
 
-app.use(morgan('dev'));
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
 
-app.get('/', (req, res) => {
-  console.log("branch commit")
-  console.log("markus commit")
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
-  });
-});
+
+// app.use(morgan('dev'));
+// app.use(helmet());
+// app.use(cors());
+// app.use(express.json());
+
+// app.use('/api/v1', api);
+
+// app.use(middlewares.notFound);
+// app.use(middlewares.errorHandler);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/testIndex.html');
 });
 
-app.use('/api/v1', api);
+io.on('connection', (socket) => {
+  console.log('yo');
+});
 
-app.use(middlewares.notFound);
-app.use(middlewares.errorHandler);
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log('yor')
+  console.log(`Listening: http://localhost:5000`);
+  /* eslint-enable no-console */
+});
 
-module.exports = app;
+
+
+
+
