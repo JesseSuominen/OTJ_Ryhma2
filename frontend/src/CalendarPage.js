24500c65e5c726033abc64905e7e549e87ad5307
Miika Hupli <miika.hupli@tuni.fi>
2024-03-22 13:30:08 +0200

    Reverse to development

--- frontend/src/CalendarPage.js
+++
@@ -1,9 +0,0 @@
-function CalendarPage () {
-  return (
-    <div>
-      <h1>This is the Calendar</h1>
-    </div>
-  );
-};
-
-export default CalendarPage;
--- frontend/src/ChatRoomPage.js
+++
@@ -1,9 +0,0 @@
-function ChatRoomPage () {
-    return (
-      <div>
-        <h1>This is the Chatroom</h1>
-      </div>
-    );
-  };
-
-  export default ChatRoomPage;
