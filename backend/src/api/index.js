ef173ec9b34904acb5478c0599741d13a4bffc5f
Miika Hupli <miika.hupli@tuni.fi>
2024-04-15 11:16:44 +0300

    Add api/v1/user/chatroom/messages query by chatroom id

--- backend/src/api/index.js
+++ backend/src/api/index.js
@@ -79,9 +79,7 @@ router.post('/v1/user', (req, res) => {
   });
 });
 
-module.exports = router;
-
-// Returns messages of a chatroom
+// Returns info of a chatroom by chatroom id
 // curl --silent --include "http://localhost:5000/api/v1/chatroom?id=1"
 router.get('/v1/user/chatroom', (req, res) => {
   const { id } = req.query;
@@ -107,3 +105,37 @@ router.get('/v1/user/chatroom', (req, res) => {
     res.status(HTTP_STATUS_OK).json(row);
   });
 });
+
+// Returns messages of a chatroom by chatroom id
+// curl --silent --include "http://localhost:5000/api/v1/chatroom/messages?id=1"
+router.get('/v1/user/chatroom/messages', (req, res) => {
+  const { id } = req.query;
+  if (!id) {
+    return res.status(BAD_REQUEST).json({ error: 'Chatroom id parameter is required' });
+  }
+  const SQL_SELECT = `
+    SELECT      u.username
+                AS "sender"
+                , m.datetime
+                , m.text
+                AS "message"
+    FROM        message AS m
+    INNER JOIN  chatroom AS c
+            ON  m.chatroom_id = c.id
+    INNER JOIN  user AS u
+            ON  m.user_id = u.id
+    WHERE       c.id = ?
+  `;
+  db.get(SQL_SELECT, [id], (err, row) => {
+    if (err) {
+      console.error('Error fetching chatroom:', err);
+      return res.status(INTERNAL_ERROR).json({ error: 'Something unexpected went wrong' });
+    }
+    if (!row) {
+      return res.status(HTTP_STATUS_NOK).json({ error: 'Chatroom not found' });
+    }
+    res.status(HTTP_STATUS_OK).json(row);
+  });
+});
+
+module.exports = router;
