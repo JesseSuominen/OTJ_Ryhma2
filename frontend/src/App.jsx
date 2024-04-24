0c2f89e4833dc7beff7b7249bbef9c800bf4b9a5
Jesse Suominen <jesse.suominen@tuni.fi>
2024-04-24 12:05:16 +0300

    calendarPageProgress

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-
-import './App.css'
 import Calendar from './pages/Calendar'
 import Chat from './pages/Chat'
 import Home from './pages/Home'
 import RootLayout from './pages/RootLayout'
+import { subDays } from 'date-fns'
 
 const router = createBrowserRouter([
   { path: '/',
     element: <RootLayout />,
     children: [
       { index: true, element: <Home/>},
-      { path: '/calendar', element: <Calendar />},
+      { path: '/calendar', element: <Calendar events={[
+        {date: subDays(new Date(),6), title: "Meeting"},
+        {date: subDays(new Date(),1), title: "Coding"},
+        {date: subDays(new Date(),3), title: "Ready?"},
+      ]}/>},
       { path: '/chat', element: <Chat />},
     ]  
   }
@@ -21,8 +24,11 @@ const router = createBrowserRouter([
 function App() {
 
   return (
-    <RouterProvider router={router}/>
+    <div>
+      <RouterProvider router={router}/>
+    </div>
   )
+    
 }
 
 export default App
--- frontend/src/pages/Calendar.jsx
+++ frontend/src/pages/Calendar.jsx
@@ -1,9 +1,80 @@
+import { useState } from 'react';
+import { eachDayOfInterval, endOfMonth, format, getDay, isToday, startOfMonth } from "date-fns";
+import "./Calendar.css";
+import CircularProgressBar from '../components/CircularProgressBar';
+import ButtonAddEvent from '../components/ButtonWithPopup';
+
+const WEEKDAYS = ["Sun", "Mon", "Tue", "Wen", "Thu", "Fri", "Sat"];
+/*
+interface Event {
+    date: Date;
+    title: String;
+    description: String;
+}
+
+interface CalendarProps {
+    events: Event[];
+}
+*/
+
 const Calendar = () => {
-    return (
-        <>
-            <h1>Calendar</h1>
-        </>
-    )
+
+  const currentDate = new Date();
+  const firstDayOfMonth = startOfMonth(currentDate);
+  const lastDayOfMonth = endOfMonth(currentDate);
+
+  const daysInMonth = eachDayOfInterval({
+    start: firstDayOfMonth,
+    end: lastDayOfMonth,
+  });
+
+  const startingDayIndex = getDay(firstDayOfMonth);
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const buttonStyle = {
+    border: '1px solid black',
+    backgroundColor: 'white',
+    padding: '8px 16px',
+    cursor: 'pointer'
+  };
+
+  const hoveredButtonStyle = {
+    border: '2px solid black',
+    backgroundColor: 'lightgrey',
+    padding: '8px 16px',
+    cursor: 'pointer'
+  };
+
+  return (
+    <div className="grid_calendar">
+      <div className="progress_stats">
+        <CircularProgressBar/>
+      </div>
+
+      <h2 className="month_and_year">{format(currentDate, "MMMM yyyy")}</h2>
+
+      <div className="grid_weekdays">
+        {WEEKDAYS.map((day) => {
+          return <div key={day}>{day}</div>;
+        })}
+
+        {Array.from({ length: startingDayIndex }).map((_, index) => {
+          return <div key={`empty- ${index}`}/>
+        })}
+
+        {daysInMonth.map((day, index) => {
+          return <div key={index}>
+            {format(day, "d")}
+          </div>;
+        })}
+      </div>
+      <ButtonAddEvent
+        onMouseOver={() => setIsHovered(true)}
+        onMouseOut={() => setIsHovered(false)}
+        style={isHovered ? hoveredButtonStyle : buttonStyle}/>
+    </div>
+  )
 }
 
 export default Calendar;
